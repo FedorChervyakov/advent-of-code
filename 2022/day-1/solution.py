f40dd07fc25c29f441322c9ecf7c8fca11749062
Fedor Chervyakov <fchervyakov@gmail.com>
2022-12-05 07:16:40 +0300

    2022: day-1: add solution to part 2
    Also, the first solution had a flaw in which we didn't consider the last
    elf.

--- 2022/day-1/solution.py
+++ 2022/day-1/solution.py
@@ -1,26 +1,46 @@
+import heapq
+
 def main(filename):
     print(f'Scanning file {filename}')
-    current_elf = 1
-    best_elf = 1
+
+    # top n elves
+    n = 3
+    # first is max
+    best_elves = [(0, 0) for _ in range(n)]
 
     current_sum = 0
-    max_calories = 0
+    current_elf = 1
+
+    def update_best_elves(line):
+        nonlocal current_sum, current_elf, best_elves
+        line = line.strip()
+
+        if len(line) == 0:
+            for i in range(n):
+                if current_sum > best_elves[i][0]:
+                    best_elves.insert(i, (current_sum, current_elf))
+                    break
+
+            best_elves = best_elves[:3]
+
+            current_sum = 0
+            current_elf += 1
+        else:
+            current_sum += int(line)
 
     with open(filename, 'r') as f:
         for line in f:
-            line = line.strip()
+            update_best_elves(line)
 
-            if len(line) == 0:
-                if current_sum > max_calories:
-                    best_elf = current_elf
-                    max_calories = current_sum
+        update_best_elves('')
 
-                current_sum = 0
-                current_elf += 1
-            else:
-                current_sum += int(line)
+    total = 0
 
-    print(f'Elf {best_elf} is the best. They carry {max_calories}!')
+    for ix, (max_calories, best_elf) in enumerate(best_elves):
+        total += max_calories
+        print(f'Elf {best_elf} is the {ix+1}. They carry {max_calories}!')
+
+    print(f'In total best elves carry {total} calories')
 
 
 
